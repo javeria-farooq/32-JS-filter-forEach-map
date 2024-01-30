277282319e98624b3ae81835372b3837f1489bb6
javeria-farooq <gettingeducation6@gmail.com>
2024-01-30 15:44:02 +0500

    32 JS Assignment

---
+++ app.js
@@ -0,0 +1,23 @@
+// filter:
+// let arr = [1,2,3,4,5,5,6,67,7,88,9,9,78]
+// let arrFilter = arr.filter(function (value) {
+//     return value > 6 // pta nh q is step pe return kye bgher doc pe console pe print nh hrh or
+//     // sr ne bh is baare mn kch nh btaya
+// })
+
+// console.log(arrFilter) 
+
+
+
+// forEach loop:
+
+// let arr = ['javeria', 'aliza', 'afnan', 'yasmeen', 'farooq']
+// arr.forEach(function (value) {
+//     document.write(value + '<br>')
+// })
+
+// map:
+// arr.map(function (value) {
+//     document.write(value + '<br>')
+// })  //map bh kaam forEach wla hi krta h bs ismn frq ye h k ye slice ki trhn new array mn value
+// ko store krta h or forEach existing array mn hi kaam krta h
